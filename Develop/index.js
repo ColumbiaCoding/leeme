3b0a170e48b6cf66d8f1f29102ca6e54bcdb253b
unknown <gabriel.regus@gmail.com>
2023-08-04 01:58:10 +0000

    adding js files

---
+++ Develop/index.js
@@ -0,0 +1,49 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// TODO: Create an array of questions for user input
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the name of the project?',
+    },
+    {
+      type: 'checkbox',
+      message: 'What languages do you know?',
+      name: 'stack',
+      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+    },
+    {
+      type: 'list',
+      message: 'What is your preferred method of communication?',
+      name: 'contact',
+      choices: ['email', 'phone', 'telekinesis'],
+    },
+  ])
+  .then((data) => {
+    console.log(data);
+    generateMarkdown(data);
+    // const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+    fs.writeFile('test.txt', JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
+
+// TODO: Create a function to write README file
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('Success!')
+  );
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+
